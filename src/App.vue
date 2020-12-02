2d6abe316e6d0cd9d3c85444d3eff8c7909a1c65
knowhere1998 <darekar.amey@gmail.com>
2020-12-02 19:04:10 +0530

    Removed init boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<Nav :nav_link.sync="active_nav_link" @changeNavLink="active_nav_link = $event"/>
+		<div id="data">
+			<div v-if="active_nav_link == 1">
+				<Quiz />
+			</div>
+			<div v-else-if="active_nav_link == 2">
+				## About
+			</div>
+			<div v-else-if="active_nav_link == 3">
+				Contact
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Quiz from './components/Quiz.vue'
+import Nav from './components/Nav.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'App',
+	components: {
+		Quiz,
+		Nav
+	},
+	data() {
+		return {
+			active_nav_link : 1
+		};
+	},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+	background: #fd746c;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
 }
 </style>
